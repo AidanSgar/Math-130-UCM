73c216df708fc0fe9f92425f6520c8912d7a2e51
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2022-09-12 23:49:18 -0700

    Add files via upload

--- presicion.m
+++ presicion.m
@@ -9,25 +9,19 @@ for i = 0:2^exponentBits-1
 end
 
 possibleExp = [-flip(possibleExp),possibleExp];
-possibleMantissaDec = 0:2^mantissaBits-1;
-possibleMantissaBi = int2bit(possibleMantissaDec,mantissaBits);
-possibleMantissa = [];
 
-for j = 1:2^mantissaBits
-    temp = 0;
-    for i = 1:mantissaBits
-        temp = temp + (possibleMantissaBi(i,j))*((1/2)^(i-1));
-    end
-    possibleMantissa = [possibleMantissa,temp];
+possibleMan = []
+for i = 0:2^mantissaBits-1
+    possibleMan = [possibleMan,2^(-mantissaBits)*i];
 end
 
 possibleValues = [];
 
 for i = 1:2^(exponentBits+1)
-    possibleValues = [possibleValues,possibleMantissa.*possibleExp(i)];
+    possibleValues = [possibleValues,(1.+possibleMan).*possibleExp(i)];
 end
 
 possibleValues = sort(possibleValues);
 possibleValues = unique(possibleValues,'stable')
 
-scatter(possibleValues,zeros(length(possibleValues)),"filled")
+plot(possibleValues)
